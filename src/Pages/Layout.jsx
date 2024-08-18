4621d5b195d68c443e874f0b57c48b5ee2f3a744
Afaf6 <afaftawfe2000@gmail.com>
2024-08-19 01:37:02 +0300

    main

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Component/Navbar/Navbar";
+import Navbar from "./Component/Navbar/Navbar";
 import './Layout.css'
 
 function Layout() {
